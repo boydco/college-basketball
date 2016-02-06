7ac07cadb9cb97b2a8f888f1c5787cd0463c25c0
colin boyd <email": null>
2016-02-06 17:09:50 +1300

    added mouseover/mouseout for top row :pineapple:

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+
+
+// function hover(element) {
+//     element.setAttribute('src', 'img/izzo-msu-green.jpg');
+// }
+// function unhover(element) {
+//     element.setAttribute('src', 'img/izzo-msu.jpeg');
+// }
